8e846c4f23b0cf7515a9588269e3821dfa95a89f
iwasakiyuki <iwasakiyuki0824@gmail.com>
2023-02-05 22:09:44 +0900

    fix node_count

--- GCP/gke.tf
+++ GCP/gke.tf
@@ -18,7 +18,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "my-node-pool"
   location   = "us-central1"
   cluster    = google_container_cluster.primary.name
-  node_count = 3
+  node_count = 1
 
   node_config {
     preemptible  = true
