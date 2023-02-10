95423312f63a8b028cb1024596976a14a1a14c6b
unknown <iwasakiyuki0824@gmail.com>
2023-02-10 22:31:02 +0900

    gke_private

--- GCP/variables.tf
+++ GCP/variables.tf
@@ -3,6 +3,11 @@ variable "region" {
   default = "us-central1"
 }
 
+variable "zone" {
+  type    = string
+  default = "us-central1-a"
+}
+
 variable "project_id" {
   type    = string
   default = "project-id"
@@ -16,4 +21,14 @@ variable "private_network_name" {
 variable "public_network_name" {
   type    = string
   default = "public-network"
+}
+
+variable "gke_cluster_control_plane_ip_range" {
+  type    = string
+  default = "172.16.0.0/28"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "private-gke"
 }
--- GCP/vpc.tf
+++ GCP/vpc.tf
@@ -16,6 +16,15 @@ resource "google_compute_subnetwork" "private_network_subnet" {
   ip_cidr_range = "10.10.20.0/24"
   region        = var.region
   network       = google_compute_network.private_network.self_link
+  private_ip_google_access = true
+  secondary_ip_range {
+    range_name    = "services"
+    ip_cidr_range = "10.10.21.0/24"
+  }
+  secondary_ip_range {
+    range_name    = "pods"
+    ip_cidr_range = "10.20.0.0/16"
+  }
 }
 
 resource "google_compute_subnetwork" "public_network_subnet" {
@@ -27,8 +36,8 @@ resource "google_compute_subnetwork" "public_network_subnet" {
 
 #### firewall ####
 resource "google_compute_firewall" "public_network_allow_ingress_internal" {
-  name          = "public-network-allow-ingress-internal"
-  network       = google_compute_network.public_network.self_link
+  name          = "${var.public_network_name}-allow-ingress-internal"
+  network       = google_compute_network.private_network.self_link
   source_ranges = ["10.0.0.0/8"]
 
   allow {
@@ -44,6 +53,24 @@ resource "google_compute_firewall" "public_network_allow_ingress_internal" {
     protocol = "udp"
     ports    = ["0-65535"]
   }
+}
+
+resource "google_compute_firewall" "public_network_allow_ingress_my_instance" {
+  name          = "${var.public_network_name}-allow-ingress-bastion-instance"
+  network       = google_compute_network.public_network.self_link
+  source_ranges = ["0.0.0.0/0"]
+
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags = ["bastion"]
 }
 
 #### vpc peering ####
@@ -100,23 +127,24 @@ resource "google_compute_router" "nat_router" {
   network = google_compute_network.private_network.self_link
 }
 
-#resource "google_compute_address" "nat_address" {
-#  name    = "nat-address"
-#}
+resource "google_compute_address" "nat_address" {
+  name = "nat-address"
+}
 
 resource "google_compute_router_nat" "nat" {
-  name                                = "${var.private_network_name}-nat"
-  nat_ip_allocate_option              = "AUTO_ONLY"
-  router                              = "${var.private_network_name}-router"
-  source_subnetwork_ip_ranges_to_nat  = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-  min_ports_per_vm = 64
+  name                               = "${var.private_network_name}-nat"
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  router                             = google_compute_router.nat_router.name
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  min_ports_per_vm                   = 64
+  nat_ips                            = [google_compute_address.nat_address.self_link]
 }
 
 ### public gce instance ###
 resource "google_compute_instance" "my_pub_instance" {
-  project       = var.project_id
-  zone          = "${var.region}-a"
-  name          = "bastion"
+  project      = var.project_id
+  zone         = "${var.zone}"
+  name         = "bastion"
   machine_type = "e2-micro"
 
   tags = ["bastion"]
@@ -138,8 +166,64 @@ resource "google_compute_instance" "my_pub_instance" {
   service_account {
     scopes = ["cloud-platform"]
   }
-}   
 
-resource "google_compute_address" "bastion_static_ip" { 
+  metadata_startup_script = "apt-get install kubectl"
+
+}
+
+resource "google_compute_address" "bastion_static_ip" {
   name = "bastion-address"
+}
+
+### GKE ###
+# private google access用のegress
+resource "google_compute_firewall" "private_gke_network_allow_egress_google_apis" {
+  name               = "private-gke-network-allow-egress-google-apis"
+  network            = google_compute_network.private_network.self_link
+  direction          = "EGRESS"
+  destination_ranges = ["199.36.153.4/30"]
+
+  allow {
+    protocol = "all"
+  }
+}
+
+# コントロールプレーン用のegress
+resource "google_compute_firewall" "private_gke_network_allow_egress_masternode" {
+  name               = "private-gke-network-allow-egress-masternode"
+  network            = google_compute_network.private_network.self_link
+  direction          = "EGRESS"
+  destination_ranges = [var.gke_cluster_control_plane_ip_range]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443", "10250"]
+  }
+}
+
+# gke
+resource "google_container_cluster" "primary" {
+  name               = var.cluster_name
+  location           = var.zone
+  initial_node_count = 1
+  network            = google_compute_network.private_network.name
+  subnetwork         = google_compute_subnetwork.private_network_subnet.name
+
+  private_cluster_config {
+    enable_private_nodes    = true # 各ノードのパブリックIPを無効化
+    enable_private_endpoint = true # マスターノードのパブリックエンドポイントを無効化
+    master_ipv4_cidr_block  = var.gke_cluster_control_plane_ip_range
+  }
+
+  ip_allocation_policy {
+    cluster_secondary_range_name   = "pods"
+    services_secondary_range_name  = "services"
+  }
+
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = google_compute_subnetwork.public_network_subnet.ip_cidr_range
+      display_name = var.public_network_name
+    }
+  }
 }
